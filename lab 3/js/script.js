bc007089233fde4e835773982899d71c6f321ca5
hughcapet <bungina@arenadata.io>
2019-03-26 13:24:14 +0300

    Implement picture generation

--- lab 3/js/script.js
+++ lab 3/js/script.js
@@ -1,7 +1,12 @@
 var imgs = [];
+var imgsReady = false;
+var imgsRecieved = 0;
+var textFetched = false;
+var fetchedQuote = null;
+var lines = [];
+
 var IMG_WIDTH = 200, IMG_HEIGHT = 200;
 var CANVAS_WIDTH = 400, CANVAS_HEIGHT = 400;
-
 
 var body = document.getElementsByTagName("body")[0];
 
@@ -13,11 +18,55 @@ body.appendChild(canvas);
 var ctx = canvas.getContext("2d");
 
 function randomInt() {
-    return Math.floor(Math.random() * (Number.MAX_VALUE - Number.MIN_VALUE + 1)) + Number.MIN_VALUE;
+    return Math.floor(Math.random() * (100000 - 200000 + 1));
 }
 
+function insertText() {
+    if (imgsReady && textFetched) {
+        var space = 10;
+        var letterHeight = parseInt("10px", 0);
+        var countLines = lines.length;
+        var start = CANVAS_HEIGHT / 2 - (letterHeight + space) * countLines / 2;
+        for (var i = 0; i < lines.length; i++) {
+            ctx.fillText(lines[i], CANVAS_WIDTH / 2,
+                start + ((i + 1) * (letterHeight + space)));
+        }
+    }
+}
 
-function fetchText() {
+function clearText(rawText) {
+    var newElem = document.createElement('div');
+    newElem.innerHTML = rawText;
+    return newElem.textContent;
+}
+
+function splitText() {
+    var text = JSON.parse(fetchedQuote)[0].content;
+    var bareText = clearText(text);
+
+    var padding = 10;
+    var words = bareText.split(" ");
+    var currentLine = words[0];
+    ctx.textAlign = "center";
+    ctx.font = "15px Helvetica";
+    ctx.fillStyle = "white";
+    for (var i = 1; i < words.length; i++) {
+        var word = words[i];
+
+        var width = ctx.measureText(currentLine + " " + word).width;
+        if (width < CANVAS_WIDTH - 2 * padding) {
+            currentLine += " " + word;
+        }
+        else {
+            lines.push(currentLine);
+            currentLine = word;
+        }
+    }
+    lines.push(currentLine);
+    textFetched = true;
+}
+
+function fetchText(callback) {
     var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function () {
@@ -25,10 +74,8 @@ function fetchText() {
             if (xhr.status !== 200) {
                 alert(xhr.status + ': ' + xhr.responseText);
             } else {
-                var text = JSON.parse(xhr.responseText)[0].content;
-                var newElem = document.createElement('div');
-                newElem.innerHTML = text;
-                document.body.appendChild(newElem);
+                fetchedQuote = xhr.responseText;
+                callback();
             }
         }
     };
@@ -40,31 +87,35 @@ function fetchText() {
     xhr.send();
 }
 
-
-function fetchImgs() {
-    for (var i = 0; i < 4; i++) {
-        imgs[i] = new Image();
-        imgs[i].src = "https://source.unsplash.com/random/200x200?random=" + randomInt();
-        imgs[i].onload = function () {
-            console.log("Success");
-        }
+function drawImgs() {
+    for (var i = 0; i < 4; i++){
+        ctx.drawImage(imgs[i], i % 2 * IMG_WIDTH, Math.floor((i + 1) / 3) * IMG_HEIGHT, IMG_WIDTH, IMG_HEIGHT);
     }
+    imgsReady = true;
+    insertText();
 }
 
-function drawImgs() {
-    for (var i = 0; i < 4; i++) {
-        imgs[i].onload = (function (e) { return function(){
-            ctx.drawImage(imgs[e], e % 2 * IMG_WIDTH, Math.floor((e + 1) / 3) * IMG_HEIGHT, IMG_WIDTH, IMG_HEIGHT);
+function fetchImgs() {
+    imgsRecieved++;
+    imgs.push(new Image());
+    imgs[imgsRecieved - 1].onerror = function() {
+        console.log("failed to fetch an image");
+        fetchImgs();
+    };
+    imgs[imgsRecieved - 1].onload = function() {
+        if (imgs.length < 4) {
+            fetchImgs();
         }
-        })(i);
-        console.log(i % 2, Math.floor((i + 1) / 3))
-    }
+        else {
+            drawImgs();
+        }
+    };
+    imgs[imgsRecieved - 1].src = "https://source.unsplash.com/collection/" + randomInt();
 }
 
 function generatePic() {
     fetchImgs();
-    fetchText();
-    drawImgs();
+    fetchText(splitText);
 }
 
 generatePic();
